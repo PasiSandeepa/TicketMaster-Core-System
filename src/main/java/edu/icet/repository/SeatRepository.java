510123a5a8617ad3a7165183330a5721a0bf34b5
PasiSandeepa <pasisandeepa456@gmail.com>
2026-01-20 10:45:25 +0530

    add SeatRepository interface

---
+++ src/main/java/edu/icet/repository/SeatRepository.java
@@ -0,0 +1,9 @@
+package edu.icet.repository;
+
+import edu.icet.model.entity.SeatEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SeatRepository extends JpaRepository<SeatEntity, Long> {
+}
