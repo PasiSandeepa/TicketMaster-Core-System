447d70554bdf30ffad5d5a47a66a56a55109ddd0
PasiSandeepa <pasisandeepa456@gmail.com>
2026-01-20 10:23:24 +0530

    add UserEntity class

---
+++ src/main/java/edu/icet/model/entity/UserEntity.java
@@ -0,0 +1,16 @@
+package edu.icet.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String tier;
+}
